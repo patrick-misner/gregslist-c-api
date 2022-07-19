c3b1887736125c69551ccf42eb3cadae8969e3a7
Patrick Misner <misner.patrick@gmail.com>
2022-07-19 14:12:21 -0600

    delete

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
- using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -14,101 +14,101 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MySqlConnector;
-using gregslist-c-api.Repositories;
-using gregslist-c-api.Services;
+using gregslist-c - api.Repositories;
+using gregslist-c - api.Services;
 
 namespace gregslist-c-api
 {
     public class Startup
+{
+  public Startup(IConfiguration configuration)
+  {
+    Configuration = configuration;
+  }
+
+  public IConfiguration Configuration { get; }
+
+  // This method gets called by the runtime. Use this method to add services to the container.
+  public void ConfigureServices(IServiceCollection services)
+  {
+    ConfigureCors(services);
+    ConfigureAuth(services);
+    services.AddControllers();
+    services.AddSwaggerGen(c =>
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+      c.SwaggerDoc("v1", new OpenApiInfo { Title = "gregslist-c-api", Version = "v1" });
+    });
+    // services.AddScoped<IDbConnection>(x => CreateDbConnection());
 
-        public IConfiguration Configuration { get; }
+    // services.AddScoped<AccountsRepository>();
+    // services.AddScoped<AccountService>();
+  }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            ConfigureCors(services);
-            ConfigureAuth(services);
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "gregslist-c-api", Version = "v1" });
-            });
-            services.AddScoped<IDbConnection>(x => CreateDbConnection());
-            
-            services.AddScoped<AccountsRepository>();
-            services.AddScoped<AccountService>();
-        }
-
-        private void ConfigureCors(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsDevPolicy", builder =>
-                {
-                    builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithOrigins(new string[]{
+  private void ConfigureCors(IServiceCollection services)
+  {
+    services.AddCors(options =>
+    {
+      options.AddPolicy("CorsDevPolicy", builder =>
+              {
+          builder
+                  .AllowAnyMethod()
+                  .AllowAnyHeader()
+                  .AllowCredentials()
+                  .WithOrigins(new string[]{
                         "http://localhost:8080", "http://localhost:8081"
-                    });
-                });
             });
-        }
+        });
+    });
+  }
 
-        private void ConfigureAuth(IServiceCollection services)
-        {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.Authority = $"https://{Configuration["AUTH0_DOMAIN"]}/";
-                options.Audience = Configuration["AUTH0_AUDIENCE"];
-            });
+  private void ConfigureAuth(IServiceCollection services)
+  {
+    services.AddAuthentication(options =>
+    {
+      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+      options.Authority = $"https://{Configuration["AUTH0_DOMAIN"]}/";
+      options.Audience = Configuration["AUTH0_AUDIENCE"];
+    });
 
-        }
+  }
 
-        private IDbConnection CreateDbConnection()
-        {
-            string connectionString = Configuration["CONNECTION_STRING"];
-            return new MySqlConnection(connectionString);
-        }
-
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "gregslist-c-api v1"));
-                app.UseCors("CorsDevPolicy");
-            }
-
-            app.UseHttpsRedirection();
-            
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
+  private IDbConnection CreateDbConnection()
+  {
+    string connectionString = Configuration["CONNECTION_STRING"];
+    return new MySqlConnection(connectionString);
+  }
 
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+  {
+    if (env.IsDevelopment())
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "gregslist-c-api v1"));
+      app.UseCors("CorsDevPolicy");
     }
+
+    app.UseHttpsRedirection();
+
+    app.UseDefaultFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+
+    app.UseEndpoints(endpoints =>
+    {
+      endpoints.MapControllers();
+    });
+  }
+}
 }
